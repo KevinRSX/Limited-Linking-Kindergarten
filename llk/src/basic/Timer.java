8ba7f983a72a7117a1a7b77857abbd9aa03d3dbd
Sun Chengyu <sun.990502@gmail.com>
2019-10-17 23:26:41 +0800

    timer v.1.0

--- llk/src/basic/Timer.java
+++ llk/src/basic/Timer.java
@@ -1,5 +1,69 @@
 package basic;
 
-public class Timer {
-
+public class Timer extends Thread {
+	private int set_time;
+	private int display_time;
+	private long duration_time;
+	private long creation_time;
+	private long finish_time;
+	
+	public Timer(int rt) {
+		set_time = display_time = rt;
+		duration_time = rt * 1000;
+	}
+	
+	public Timer(int rt, long et) {
+		set_time = rt;	
+		duration_time = rt * 1000 - et;
+		display_time = (int)(duration_time / 1000);
+	}
+	
+	@Override
+	public void run() {
+		creation_time = System.currentTimeMillis();
+		finish_time = creation_time + duration_time;
+		long temp = creation_time;
+		System.out.println(display_time);
+		while(!Thread.currentThread().isInterrupted()) {
+			if (System.currentTimeMillis() - temp == 1000) {
+				System.out.println(--display_time);
+				temp = System.currentTimeMillis();
+			}
+			if (System.currentTimeMillis() >= finish_time) {
+				break;
+			}
+		}
+		// successfully finished task, notify Game
+	}
+	
+	public long getElapsedTime() {
+		return (System.currentTimeMillis() - creation_time);
+	}
+	/*
+	private class TimerThread extends Thread {
+		private int re_time_sec = 0;
+		private long creation_time;
+		private long finish_time;
+		
+		private TimerThread(int rt) {
+			re_time_sec = rt;
+		}
+		
+		@Override
+		public void run() {
+			creation_time = System.currentTimeMillis();
+			finish_time = creation_time + re_time_sec * 1000;
+			long temp = creation_time;
+			System.out.println(re_time_sec);
+			while(true) {
+				if (System.currentTimeMillis() - temp == 1000) {
+					System.out.println(--re_time_sec);
+					temp = System.currentTimeMillis();
+				}
+				if (System.currentTimeMillis() >= finish_time)
+					break;
+			}
+			// successfully finished task, notify Game
+		}
+	}*/
 }
--- llk/src/basic/test.java
+++ llk/src/basic/test.java
@@ -1,8 +1,164 @@
 package basic;
+import java.util.Scanner;
 
 public class test {
-	public static void main(String[] args)
-	{
-		System.out.println("fuck Kevin");
+	public static void main(String[] args) {
+		TimerHelper t = TimerHelper.getInstance();
+		t.setUp(100);
+		t.start();
+		Scanner sc = new Scanner(System.in);
+		char in;
+		in = sc.next().charAt(0);
+		while(in != 'e') {
+			switch (in) {
+			case 'p':
+				t.pause();
+				break;
+			case 'r':
+				t.restart();
+				break;
+			case 't':
+				t.terminate();
+				break;
+			case 'i':
+				int to_increase = sc.nextInt();
+				t.increaseTime(to_increase);
+				break;
+			case 'd':
+				int to_decrease = sc.nextInt();
+				t.decreaseTime(to_decrease);
+				break;
+			}
+			in = sc.next().charAt(0);
+		}
+	}
+	
+	public static class TimerHelper {
+		private static TimerHelper tp = new TimerHelper();
+		private Timer timer;
+		private int set_time;
+		private int bonus_time;
+		private long elapsed_time;
+		private boolean is_paused;
+		private boolean is_running;
+		
+		private TimerHelper() {
+			set_time = bonus_time = 0;
+			elapsed_time = 0L;
+			is_running = is_paused = false;
+		}
+		
+		public static TimerHelper getInstance(){
+			return tp;
+		}
+		
+		public boolean setUp(int rt) {
+			set_time = rt;
+			elapsed_time = 0L;
+			timer = new Timer(rt);
+			return true;
+		}
+		
+		public boolean start() {
+			if (!is_paused && !is_running) {
+				timer.start();
+				System.out.println("started.");
+				is_running = true;
+				return true;
+			} else {
+				// throw exceptions
+				System.out.println("start error.");
+				return false;
+			}
+			
+		}
+		
+		public boolean pause() {
+			if (is_running && !is_paused) {
+				elapsed_time += timer.getElapsedTime();
+				timer.interrupt();
+				System.out.println("paused.");
+				is_paused = true;
+				is_running = false;
+				return true;
+			} else {
+				// throw exceptions
+				System.out.println("pause error.");
+				return false;
+			}
+			
+		}
+		
+		public boolean restart() {
+			if (is_paused && !is_running) {
+				timer = new Timer(set_time, elapsed_time);
+				timer.start();
+				System.out.println("restarted.");
+				is_paused = false;
+				is_running = true;
+				return true;
+			} else {
+				// throw exceptions
+				System.out.println("restart error.");
+				return false;
+			}
+		}
+		
+		public boolean terminate() {
+			if (is_paused && !is_running) {
+				System.out.println("terminated.");
+				is_paused = false;
+				return true;
+			} else if (is_running && !is_paused) {
+				timer.interrupt();
+				System.out.println("terminated.");
+				is_running = false;
+				return true;
+			} else {
+				// throw exceptions
+				System.out.println("terminate error.");
+				return false;
+			}			
+		}
+		
+		public boolean increaseTime(int it) {
+			if (is_running) {
+				elapsed_time += timer.getElapsedTime();
+				timer.interrupt();
+				elapsed_time = (it * 1000 > elapsed_time) ? 0 : elapsed_time - it*1000;
+				timer = new Timer(set_time, elapsed_time);
+				timer.start();
+				System.out.println("added " + it + " seconds to timer.");
+				return true;
+			} else if (is_paused) {
+				elapsed_time = (it * 1000 > elapsed_time) ? 0 : elapsed_time - it*1000;
+				System.out.println("added " + it + " seconds to timer.");
+				return true;
+			} else {
+				// throw exceptions
+				System.out.println("increase time error.");
+				return false;
+			}		
+		}
+		
+		public boolean decreaseTime(int dt) {
+			if (is_running) {
+				elapsed_time += timer.getElapsedTime();
+				timer.interrupt();
+				elapsed_time = (dt * 1000 > (set_time * 1000 - elapsed_time)) ? set_time * 1000 : elapsed_time + dt*1000;
+				timer = new Timer(set_time, elapsed_time);
+				timer.start();
+				System.out.println("subtracted " + dt + " seconds to timer.");
+				return true;
+			} else if (is_paused) {
+				elapsed_time = (dt * 1000 > (set_time * 1000 - elapsed_time)) ? set_time * 1000 : elapsed_time + dt*1000;
+				System.out.println("subtracted " + dt + " seconds to timer.");
+				return true;
+			} else {
+				// throw exceptions
+				System.out.println("decrease time error.");
+				return false;
+			}	
+		}
 	}
 }
